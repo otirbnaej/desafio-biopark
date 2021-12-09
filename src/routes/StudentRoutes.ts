a57e951be7c2f893b6e581eb099b25668c96f292
Jean Brito <otirbnaej@hotmail.com>
2021-12-09 14:16:33 -0300

    insert routes to create students

---
+++ src/routes/StudentRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { CreateStudentController } from '@controllers/student/CreateStudentController';
+// import { ListStudentController } from '@controllers/student/ListStudentController';
+
+const router = Router();
+
+// const listStudentController = new ListStudentController();
+const createStudentController = new CreateStudentController();
+
+// Student routes
+// router.get('/students', listStudentController.handle);
+router.post('/student', createStudentController.handle);
+
+export { router };
