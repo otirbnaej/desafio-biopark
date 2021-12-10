b9d062a29662cac08743985cc4fdc8bb0390de71
Jean Brito <otirbnaej@hotmail.com>
2021-12-10 13:14:39 -0300

    add validations

--- src/services/student/CreateStudentService.ts
+++ src/services/student/CreateStudentService.ts
@@ -1,14 +1,34 @@
+import prismaClient from '../../prisma';
+
 interface IStudentRequest {
 	name: string;
 	email: string;
 	cpf: string;
-	birthDate: Date;
+	birthDate: string;
 }
 
 class CreateStudentService {
 	async execute({ name, email, cpf, birthDate }: IStudentRequest) {
 		try {
-			const student = { name, email, cpf, birthDate };
+			if (!name) throw `Student must have a name.`;
+			if (!email) throw `Student must have an email.`;
+			if (!birthDate) throw `Student must have a birth date`;
+
+			// Validate Date
+			const [day, month, year] = birthDate.split('-');
+			console.log(day, month, year);
+
+			const date = new Date(`${year}-${month}-${day}`);
+			console.log(date);
+
+			const student = await prismaClient.student.create({
+				data: {
+					name,
+					email,
+					cpf,
+					birthDate: date,
+				},
+			});
 
 			return student;
 		} catch (err) {
