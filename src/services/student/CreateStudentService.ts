ef0472276da697888fd9a8aa55ef2a8367a34224
Jean Brito <otirbnaej@hotmail.com>
2021-12-10 19:46:49 -0300

    include validator to check values

--- src/services/student/CreateStudentService.ts
+++ src/services/student/CreateStudentService.ts
@@ -1,5 +1,6 @@
 import prismaClient from '../../prisma';
 import validator from 'validator';
+import { CPF } from 'src/helpers/CPF';
 
 interface IStudentRequest {
 	name: string;
@@ -11,26 +12,34 @@ interface IStudentRequest {
 class CreateStudentService {
 	async execute({ name, email, cpf, birthDate }: IStudentRequest) {
 		try {
-			if (!name) throw `Student must have a name.`;
-			if (!email) throw `Student must have an email.`;
-			if (!validator.isEmail(email)) throw `Invalid email.`;
-
+			// Checks if student already exists
 			const studentAlreadyExists = await prismaClient.student.findUnique({
 				where: {
 					email,
 				},
 			});
 			if (studentAlreadyExists) throw `Student's email already exists.`;
-			if (!birthDate) throw `Student must have a birth date`;
+
+			// Validate Empty Fields
+			if (!name || validator.isEmpty(name)) throw `Student must have a name.`;
+			if (!email || validator.isEmpty(email))
+				throw `Student must have an email.`;
+			if (!validator.isEmail(email)) throw `Invalid email.`;
 
 			// Validate Date
+			if (!birthDate || validator.isEmpty(birthDate))
+				throw `Student must have a birth date`;
 			const [day, month, year] = birthDate.split('-');
 			if (!validator.isDate(`${year}-${month}-${day}`))
 				throw `Invalid birthdate`;
-			console.log(day, month, year);
-
 			const date = new Date(`${year}-${month}-${day}`);
-			console.log(date);
+
+			// Validate CPF
+			if (!cpf || validator.isEmpty(cpf)) throw 'Student must have a CPF';
+			if (!/^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(cpf)) throw 'Invalid CPF format';
+			const newCpf = new CPF(cpf);
+			const cpfIsValid = newCpf.validate();
+			if (!cpfIsValid) throw 'Invalid CPF';
 
 			const student = await prismaClient.student.create({
 				data: {
